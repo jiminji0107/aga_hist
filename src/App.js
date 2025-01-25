856477f97067405d7b2cc9c44841554349ea6e6b
jiminji <sionyjm@gmail.com>
2025-01-25 13:22:33 +0900

    design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css'; // CSS 파일 추가
+import './App.css'; // CSS 파일
 import profile1 from './an.png';
 import profile2 from './um.png';
 
@@ -21,17 +21,16 @@ const App = () => {
   const profiles = [
     {
       name: '안희주 센터장',
-      image: profile1, 
+      image: profile1,
       details: [
         '한양대학교 융합대학원 미술교육 전공 박사수료',
         '한양대학교 심리상담학 석사',
-        '\n',
+        '',
         '임상심리사 2급(한국산업인력공단)',
         '청소년상담사 2급(여성가족부)',
         '게슈탈트 상담심리사 2급(한국게슈탈트상담심리학회)',
         '현) 허그하트 심리상담센터 하남미사본점 원장',
-        '...'
-        // 나머지 내용...
+        '...',
       ],
     },
     {
@@ -45,8 +44,7 @@ const App = () => {
         '스트레스 관리사 1급(한국종합교육원)',
         '감정노동관리사 1급(한국종합교육원)',
         '현) 허그하트 심리상담센터 하남미사본점 원장',
-        '...'
-        // 나머지 내용...
+        '...',
       ],
     },
   ];
@@ -55,6 +53,7 @@ const App = () => {
     <div className="profiles-container">
       <header className="header">AGA Hugheart</header>
       <p className="description">강사 프로필 안내</p>
+
       <div className="profile-cards-wrapper">
         {profiles.map((profile, index) => (
           <ProfileCard
@@ -65,14 +64,15 @@ const App = () => {
           />
         ))}
       </div>
+
       <a
-          className="App-link"
-          href="https://sion0107.cafe24.com/skin-base"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          돌아가기
-        </a>
+        className="App-link"
+        href="https://sion0107.cafe24.com/skin-base"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        돌아가기
+      </a>
     </div>
   );
 };
